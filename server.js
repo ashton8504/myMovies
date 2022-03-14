0ec69617f1bc5b78e0f9d09ed9616dfd927343c9
Ashton <92445005+ashton8504@users.noreply.github.com>
2022-03-14 18:56:55 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -29,21 +29,12 @@ http.createServer((request, response) => {  //request & response are the argumen
     }
     console.log('data', data)
 
-    response.writeHead(200, { 'Content-Type': 'text/plain' }); // tells the server to add a header to the response
+    response.writeHead(200, { 'Content-Type': 'text/html' }); // tells the server to add a header to the response
+    response.write(data);
     response.end('Hello Node!\n');//This ends the response and sends back the message "hello node"
   });
   
 }).listen(8080);//this listens for requests on port 8080
 console.log('My first Node test server is running on Port 8080.');
 
-
-  //fs (file system) module follows the url
-
-  // const fs = require("fs");
-  //   fs.readFile('input.txt', (err, data) => {
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     console.log('File content: ' + data.toString());
-  //   });
 

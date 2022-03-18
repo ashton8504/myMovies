953858a261185a8f6bdb52372492f0d76a1bddd9
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-03-18 16:25:57 -0400

    testing

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ app.delete('/users/:id', (req, res) => {
 
   if (user) {
     users = users.filter(user => user.id != id);
-    res.status(200).send(`user ${id} has been delete`);
+    res.status(200).send(`user ${id} has been deleted`);
   } else {
     res.status(400).send('User not found.')
   }

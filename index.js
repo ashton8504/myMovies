467897906760efdc355705e6300aae8ca509970d
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-04-04 13:33:56 -0400

    more updates

--- index.js
+++ index.js
@@ -96,17 +96,17 @@ app.get('/users', passport.authenticate('jwt', { session:false }), (req, res) =>
         });
 });
 
-// // Get a user by username
-// app.get('/users/:Username', passport.authenticate('jwt', { session:false }), (req, res) => {
-//     Users.findOne({ Username: req.params.Username })
-//         .then((user) => {
-//             res.json(user);
-//         })
-//         .catch((err) => {
-//             console.error(err);
-//             res.status(500).send('Error: ' + err);
-//         });
-// });
+// Get a user by username
+app.get('/users/:Username', passport.authenticate('jwt', { session:false }), (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
 
 app.get('/director/:Name', passport.authenticate('jwt', { session:false }), (req, res) => {
     Movies.findOne({ 'Director.Name': req.params.Name })

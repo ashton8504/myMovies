f4d0ea013794fcbd355002df4f81f698a6f2fae7
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-04-04 12:12:17 -0400

    pushing to heroku

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ app.post('/users',
         check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
         check('Password', 'Password is required').not().isEmpty(),
         check('Email', 'Email does not appear to be valid').isEmail()
-    ], (req, res) => {
+    ], (req, res) => { 
 
         // check the validation object for errors
         let errors = validationResult(req);

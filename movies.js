1cc3168d0a2bcba67b4da131e7d2025f39ed6a0d
Ashton Little <ashtonlittle@Ashtons-iMac.columbus.rr.com>
2022-04-04 14:09:42 -0400

    testing

---
+++ movies.js
@@ -0,0 +1,198 @@
+{
+    "_id" : ObjectId("623ccc80b927fae6c791689a"),
+    "Title" : "The Batman",
+    "Description" : "When the Riddler, a sadistic serial killer, begins murdering key political figures in Gotham, Batman is forced to investigate the city''s hidden corruption and question his family''s involvement.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Matt Reeves",
+        "Bio" : " Matthew George Reeves was born April 27, 1966 in Rockville Center, New York, USA and is a writer, director and producer. Reeves began making movies at age eight, directing friends and using a wind-up camera.",
+        "Birth" : "1966"
+},
+    "ImagePath" : "thebatman.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623ccd04b927fae6c791689b"),
+    "Title" : "Stand by Me",
+    "Description" : "After the death of one of his friends, a writer recounts a childhood journey with his friends to find the body of a missing boy.",
+    "Genre" : {
+    "Name" : "Drama",
+        "Description" : "In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
+},
+    "Director" : {
+    "Name" : "Rob Reiner",
+        "Bio" : "Robert Reiner was born in New York City, to Estelle Reiner (née Lebost) and Emmy-winning actor, comedian, writer, and producer Carl Reiner. As a child, his father was his role model, as Carl Reiner created and starred in The Dick Van Dyke Show. Estelle was also an inspiration for him to become a director; her experience as a singer helped him understand how music was used in a scene. Rob often felt pressured about measuring up to his father''s successful streak, with twelve Emmys and other prestigious awards.",
+        "Birth" : "1947"
+},
+    "ImagePath" : "standbyme.png",
+    "Featured" : true,
+    "Bio" : "Robert Reiner was born in New York City, to Estelle Reiner (née Lebost) and Emmy-winning actor, comedian, writer, and producer Carl Reiner."
+}
+{
+    "_id" : ObjectId("623ccda1b927fae6c791689c"),
+    "Title" : "Star Wars: Episode VI - Return of the Jedi",
+    "Description" : "After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy the second Death Star. Meanwhile, Luke struggles to help Darth Vader back from the dark side without falling into the Emperor''s trap.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Richard Marquand",
+        "Bio" : "Richard Marquand was born on September 22, 1937 in Llanishen, Cardiff, Glamorgan, Wales. He was a director and producer, known for Star Wars: Episode VI - Return of the Jedi (1983), Jagged Edge (1985) and Nowhere to Run (1993). He was married to Carol Bell and Josephine Marquand. He died on September 4, 1987 in Tunbridge Wells, Kent, England.",
+        "Birth" : "1937",
+        "Death" : "1987"
+},
+    "ImagePath" : "returnofthejedi.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623ccdeeb927fae6c791689d"),
+    "Title" : "Star Wars",
+    "Description" : "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire''s world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Richard Marquand",
+        "Bio" : "Richard Marquand was born on September 22, 1937 in Llanishen, Cardiff, Glamorgan, Wales. He was a director and producer, known for Star Wars: Episode VI - Return of the Jedi (1983), Jagged Edge (1985) and Nowhere to Run (1993). He was married to Carol Bell and Josephine Marquand. He died on September 4, 1987 in Tunbridge Wells, Kent, England.",
+        "Birth" : "1937",
+        "Death" : "1987"
+},
+    "ImagePath" : "starwars.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623cce21b927fae6c791689e"),
+    "Title" : "Star Wars: Episode V - The Empire Strikes Back",
+    "Description" : "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued across the galaxy by Darth Vader and bounty hunter Boba Fett.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Richard Marquand",
+        "Bio" : "Richard Marquand was born on September 22, 1937 in Llanishen, Cardiff, Glamorgan, Wales. He was a director and producer, known for Star Wars: Episode VI - Return of the Jedi (1983), Jagged Edge (1985) and Nowhere to Run (1993). He was married to Carol Bell and Josephine Marquand. He died on September 4, 1987 in Tunbridge Wells, Kent, England.",
+        "Birth" : "1937",
+        "Death" : "1987"
+},
+    "ImagePath" : "empire.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623cce8ab927fae6c791689f"),
+    "Title" : "Batman Begins",
+    "Description" : "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from corruption and to save the city.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Christopher Nolan",
+        "Bio" : "Best known for his cerebral, often nonlinear, storytelling, acclaimed writer-director Christopher Nolan was born on July 30, 1970, in London, England. Over the course of 15 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made.",
+        "Birth" : "1970"
+},
+    "ImagePath" : "batmanbegins.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623cced9b927fae6c79168a0"),
+    "Title" : "The Dark Knight",
+    "Description" : "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Christopher Nolan",
+        "Bio" : "Best known for his cerebral, often nonlinear, storytelling, acclaimed writer-director Christopher Nolan was born on July 30, 1970, in London, England. Over the course of 15 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made.",
+        "Birth" : "1970"
+},
+    "ImagePath" : "darkknightreturns.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623ccf12b927fae6c79168a1"),
+    "Title" : "The Dark Knight Rises",
+    "Description" : "Eight years after the Joker''s reign of anarchy, Batman, with the help of the Catwoman,is forced from his exile to save Gotham City from the brutal terrorist Bane.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Christopher Nolan",
+        "Bio" : "Best known for his cerebral, often nonlinear, storytelling, acclaimed writer-director Christopher Nolan was born on July 30, 1970, in London, England. Over the course of 15 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made.",
+        "Birth" : "1970"
+},
+    "ImagePath" : "darkknightrises.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623ccf47b927fae6c79168a2"),
+    "Title" : "Dunkirk",
+    "Description" : "Allied soldiers from Belgium, the British Commonwealth and Empire, and France are surrounded by the German Army and evacuated during a fierce battle in World War II.",
+    "Genre" : {
+    "Name" : "Action",
+        "Description" : "Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats."
+},
+    "Director" : {
+    "Name" : "Christopher Nolan",
+        "Bio" : "Best known for his cerebral, often nonlinear, storytelling, acclaimed writer-director Christopher Nolan was born on July 30, 1970, in London, England. Over the course of 15 years of filmmaking, Nolan has gone from low-budget independent films to working on some of the biggest blockbusters ever made.",
+        "Birth" : "1970"
+},
+    "ImagePath" : "dunkirk.png",
+    "Featured" : true
+}
+{
+    "_id" : ObjectId("623db52f31201a77a87dac09"),
+    "Title" : "Cool Hand Luke",
+    "Description" : "A laid back Southern man is sentenced to two years in a rural prison, but refuses to conform.",
+    "Genre" : {
+    "Name" : "Drama",
+        "Description" : "In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
+},
+    "Director" : {
+    "Name" : "Stuart Rosenberg",
+        "Bio" : "Stuart Rosenberg was born on August 11, 1927 in Brooklyn, New York City, New York, USA. He was a director and producer, known for Cool Hand Luke (1967), Question 7 (1961) and The Defenders (1961). He was married to Margot Pohoryles. He died on March 15, 2007 in Beverly Hills, Los Angeles, California, USA.",
+        "Birth" : "1927",
+        "Death" : "2007"
+},
+    "ImagePath" : "coolhandluke.png",
+    "Featured" : true
+
+
+    {
+        "_id" : ObjectId("623cd76bb927fae6c79168a3"),
+        "UserName" : "Ashton Little",
+        "Password" : "1234",
+        "Email" : "ashton@gmail.com",
+        "Birthday" : "01/01/1987",
+        "FavoriteMovies" : [
+        ObjectId("623cced9b927fae6c79168a0")
+    ]
+    }
+    {
+        "_id" : ObjectId("623cd798b927fae6c79168a4"),
+        "UserName" : "Jim Little",
+        "Password" : "483",
+        "Email" : "Jim@gmail.com",
+        "Birthday" : "01/01/1981"
+    }
+    {
+        "_id" : ObjectId("623cd7bcb927fae6c79168a5"),
+        "UserName" : "Neil Elkins",
+        "Password" : "3453",
+        "Email" : "Neil@gmail.com",
+        "Birthday" : "01/01/1986"
+    }
+    {
+        "_id" : ObjectId("623cd7d8b927fae6c79168a6"),
+        "UserName" : "Kyle Elkins",
+        "Password" : "0909",
+        "Email" : "Kyle@gmail.com",
+        "Birthday" : "01/01/1989"
+    }
+
